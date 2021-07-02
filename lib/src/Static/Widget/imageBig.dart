266c194590875ecdec4f9ed126b1a784f21f18f6
mihyeon <2_5_3_1@naver.com>
2021-07-02 10:42:58 +0900

    image slide 제스쳐

--- lib/src/Static/Widget/imageBig.dart
+++ lib/src/Static/Widget/imageBig.dart
@@ -12,128 +12,185 @@ class _ImageBigState extends State<ImageBig> {
   var image = Get.arguments['image'];
   var index = Get.arguments['index'];
   var page = Get.arguments['page'];
+  String swipeDirection;
+
   @override
   Widget build(BuildContext context) {
     return page == "reviewImage"
         ? Scaffold(
-            body: Stack(
-              children: [
-                Center(
-                  child: Container(
+            body: GestureDetector(
+              onPanUpdate: (details) {
+                swipeDirection = details.delta.dx < 0 ? 'left' : 'right';
+              },
+              onPanEnd: (details) {
+                if (swipeDirection == 'left') {
+                  //handle swipe left event
+                  print("left");
+                  if (index < image.length - 1) {
+                    setState(() {
+                      index++;
+                    });
+                  }
+                }
+                if (swipeDirection == 'right') {
+                  //handle swipe right event
+                  print("right");
+                  if (index > 0) {
+                    setState(() {
+                      index--;
+                    });
+                  }
+                }
+              },
+              child: Stack(
+                children: [
+                  Center(
                     child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                        image: DecorationImage(
-                            image: NetworkImage(image[index]["image_path"]),
-                            fit: BoxFit.contain),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          image: DecorationImage(
+                              image: NetworkImage(image[index]["image_path"]),
+                              fit: BoxFit.contain),
+                        ),
                       ),
+                      color: Colors.black,
                     ),
-                    color: Colors.black,
                   ),
-                ),
-                Center(
-                  child: Row(
-                    children: [
-                      Padding(padding: EdgeInsets.only(left: 50.w)),
-                      TextButton.icon(
-                        onPressed: () {
-                          setState(() {
-                            if (index > 0) {
-                              setState(() {
-                                index--;
-                              });
-                            }
-                          });
-                        },
-                        icon: Icon(
-                          Icons.arrow_left_outlined,
-                          size: 250.w,
-                          color: Colors.black,
-                        ),
-                        label: Text(""),
-                      ),
-                      Padding(padding: EdgeInsets.only(left: 750.w)),
-                      TextButton.icon(
-                        onPressed: () {
-                          print(index);
-                          print(image.length);
-                          if (index < image.length - 1) {
-                            setState(() {
-                              index++;
-                            });
-                          }
-                        },
-                        icon: Icon(
-                          Icons.arrow_right_outlined,
-                          size: 250.w,
-                          color: Colors.black,
-                        ),
-                        label: Text(""),
-                      )
-                    ],
-                  ),
-                )
-              ],
+                  Center(
+                    child: Row(
+                      children: [
+                        Padding(padding: EdgeInsets.only(left: 100.w)),
+                        index > 0
+                            ? InkWell(
+                                child: Image.asset(
+                                  "assets/reviewPage/left.png",
+                                  width: 80.w,
+                                ),
+                                onTap: () {
+                                  setState(() {
+                                    if (index > 0) {
+                                      setState(() {
+                                        index--;
+                                      });
+                                    }
+                                  });
+                                },
+                              )
+                            : SizedBox(
+                                width: 80.w,
+                              ),
+                        Padding(padding: EdgeInsets.only(left: 1150.w)),
+                        (index < image.length - 1)
+                            ? InkWell(
+                                child: Image.asset(
+                                  "assets/reviewPage/right.png",
+                                  width: 80.w,
+                                ),
+                                onTap: () {
+                                  if (index < image.length - 1) {
+                                    setState(() {
+                                      index++;
+                                    });
+                                  }
+                                },
+                              )
+                            : SizedBox(
+                                width: 80.w,
+                              )
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           )
         : Scaffold(
-            body: Stack(
-              children: [
-                Center(
-                  child: Container(
+            body: GestureDetector(
+              onPanUpdate: (details) {
+                swipeDirection = details.delta.dx < 0 ? 'left' : 'right';
+              },
+              onPanEnd: (details) {
+                if (swipeDirection == 'left') {
+                  //handle swipe left event
+                  print("left");
+                  if (index < image.length - 1) {
+                    setState(() {
+                      index++;
+                    });
+                  }
+                }
+                if (swipeDirection == 'right') {
+                  //handle swipe right event
+                  print("right");
+                  if (index > 0) {
+                    setState(() {
+                      index--;
+                    });
+                  }
+                }
+              },
+              child: Stack(
+                children: [
+                  Center(
                     child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                        image: DecorationImage(
-                            image: NetworkImage(image[index]),
-                            fit: BoxFit.contain),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          image: DecorationImage(
+                              image: NetworkImage(image[index]),
+                              fit: BoxFit.contain),
+                        ),
                       ),
+                      color: Colors.black,
                     ),
                   ),
-                ),
-                Center(
-                  child: Row(
-                    children: [
-                      Padding(padding: EdgeInsets.only(left: 50.w)),
-                      TextButton.icon(
-                        onPressed: () {
-                          setState(() {
-                            if (index > 0) {
-                              setState(() {
-                                index--;
-                              });
-                            }
-                          });
-                        },
-                        icon: Icon(
-                          Icons.arrow_left_outlined,
-                          size: 250.w,
-                          color: Colors.black,
-                        ),
-                        label: Text(""),
-                      ),
-                      Padding(padding: EdgeInsets.only(left: 750.w)),
-                      TextButton.icon(
-                        onPressed: () {
-                          print(index);
-                          print(image.length);
-                          if (index < image.length - 1) {
-                            setState(() {
-                              index++;
-                            });
-                          }
-                        },
-                        icon: Icon(
-                          Icons.arrow_right_outlined,
-                          size: 250.w,
-                          color: Colors.black,
-                        ),
-                        label: Text(""),
-                      )
-                    ],
-                  ),
-                )
-              ],
+                  Center(
+                    child: Row(
+                      children: [
+                        Padding(padding: EdgeInsets.only(left: 100.w)),
+                        index > 0
+                            ? InkWell(
+                                child: Image.asset(
+                                  "assets/reviewPage/left.png",
+                                  width: 80.w,
+                                ),
+                                onTap: () {
+                                  setState(() {
+                                    if (index > 0) {
+                                      setState(() {
+                                        index--;
+                                      });
+                                    }
+                                  });
+                                },
+                              )
+                            : SizedBox(
+                                width: 80.w,
+                              ),
+                        Padding(padding: EdgeInsets.only(left: 1150.w)),
+                        (index < image.length - 1)
+                            ? InkWell(
+                                child: Image.asset(
+                                  "assets/reviewPage/right.png",
+                                  width: 80.w,
+                                ),
+                                onTap: () {
+                                  if (index < image.length - 1) {
+                                    setState(() {
+                                      index++;
+                                    });
+                                  }
+                                },
+                              )
+                            : SizedBox(
+                                width: 80.w,
+                              ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           );
   }
